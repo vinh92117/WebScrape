1671608f0a47a149be5c3fdfe8e29080820a2d34
Vinh Nguyen <vinh.nguyen@perspectium.com>
2020-04-13 12:17:58 -0700

    Readded method for deprecated Processor class

--- src/main/java/com/webscrape/util/JsonUtil.java
+++ src/main/java/com/webscrape/util/JsonUtil.java
@@ -2,6 +2,7 @@ package com.webscrape.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.logging.Logger;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -24,6 +25,18 @@ public class JsonUtil {
 			throw new NullPointerException("Invalid JSON from provided contents");
 		
 		return json;
+	}
+	
+	// Process incoming JSONs by calling to their designated URLs and updating the size
+	public JsonNode processJson(JsonNode jsonInput) throws MalformedURLException, IOException {
+		String url = jsonInput.get(URL).asText();
+		int urlSize = urlUtil.getUrlFileSize(url);
+		if (Integer.parseInt(jsonInput.get(SIZE).asText()) != urlSize)
+			throw new RuntimeException("Size of page in input JSON is incorrect for URL: " + url + "\nReceived size is " + urlSize);
+		
+		((ObjectNode) jsonInput).put(SIZE, urlSize);
+		
+		return jsonInput;
 	}
 	
 	// Take the JSON input and return only the web info so that it can be stored into a JSON with their path as the key
